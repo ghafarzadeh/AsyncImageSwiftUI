91d7fa28e7bbba17bc5df40db41dcfcfd5ef11d0
Habib Ghaffarzadeh <habib.ghaffarzadeh@roofstacks.com>
2022-07-30 22:26:00 +0300

     load a remote image from a URL

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Habib Ghaffarzadeh on 30.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
